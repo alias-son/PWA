76e461609967390d9aeedfce3db72bd9dcf60163
alias-son <43431381+alias-son@users.noreply.github.com>
2021-05-15 15:58:29 +0900

    update

---
+++ 05-07/src/main_page.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+	<div>
+		<h2>Main Page!</h2>
+		<button @click="fnSubPage">move to Sub Page(라우터 함수 사용)</button>
+	</div>
+</template>
+<script>
+export default {
+	methods: {
+		fnSubPage() {
+			this.$router.push('/sub')
+		}
+	}
+} </script>
---
+++ 05-07/src/sub_page.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+	<div>
+		<h2>Sub Page!</h2>
+		<button @click="fnMainPage">move to Main Page(라우터 함수 사용)</button>
+	</div>
+</template>
+<script>
+export default {
+	methods: {
+		fnMainPage() {
+			this.$router.push('/main')
+		}
+	}
+} </script>
